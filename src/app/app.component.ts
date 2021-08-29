f08f46c0610bfce11cb629e49dd4cdbafe28647b
kayalsai <kayalanu48@gmail.com>
2021-08-29 16:03:08 +0530

    kayal-commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title="Learning!";
+  isShow=true;
+  Task="";
+  name="";
+  imgUrl="https://us.123rf.com/450wm/dazdraperma/dazdraperma1005/dazdraperma100500106/7056691-pile-book-with-red-apple-illustration-isolated-on-white-background.jpg?ver=6";
+  isLogIn : boolean = false;
+  isLogOut : boolean = true;
+  color="green";
+  names=["Anu","Abi","Keerthi","Anusri","Kayal"];
+  names1="Orange";//input
 
+
+ok()
+{
+  this.Task="Click";
+
+}
 }
